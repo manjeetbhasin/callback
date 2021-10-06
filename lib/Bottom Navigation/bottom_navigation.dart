9bf7beeec3b74cbcd6f2049d2b0d5d063a8969d6
Jhanvi Vaishnav <naman.v@indianic.com>
2021-10-06 23:33:07 +0530

    correction
    change the name .. don't use class name same as widget's name.

--- lib/Bottom Navigation/bottom_navigation.dart
+++ lib/Bottom Navigation/bottom_navigation.dart
@@ -1,18 +1,16 @@
-
-
 import 'package:callback/Model/grid_view.dart';
 import 'package:callback/list_tile.dart';
 import 'package:flutter/material.dart';
 
-class BottomNavigationBar extends StatefulWidget {
- // final int CurrentIndex;
-  const BottomNavigationBar({Key? key}) : super(key: key);
+class BottomNavigationBar1 extends StatefulWidget {
+  // final int CurrentIndex;
+  const BottomNavigationBar1({Key? key}) : super(key: key);
 
   @override
-  _BottomNavigationBarState createState() => _BottomNavigationBarState();
+  _BottomNavigationBar1State createState() => _BottomNavigationBar1State();
 }
 
-class _BottomNavigationBarState extends State<BottomNavigationBar> {
+class _BottomNavigationBar1State extends State<BottomNavigationBar1> {
   // List<Widget> widgets=[];
   // int CurrentIndex=0;
   // @override
@@ -25,38 +23,39 @@ class _BottomNavigationBarState extends State<BottomNavigationBar> {
   //
   // }
   @override
-
   Widget build(BuildContext context) {
     return Scaffold(
-      bottomNavigationBar:BottomNavigationBar(
-          items:[
-            BottomNavigationBarItem(
-            icon: Icon(Icons.account_circle,size: 35,),
-            label: "Students",
-              tooltip: "Students Details",
-            backgroundColor: Colors.purpleAccent,
-
-          ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.add_photo_alternate,size: 35,),
-              label: "Flowers",
-              tooltip: "Flower Images",
-              backgroundColor: Colors.orangeAccent,
-
+      bottomNavigationBar: BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.account_circle,
+              size: 35,
             ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.notifications_active,size: 35,),
-              label: "Notification",
-              tooltip: "Notification",
-              backgroundColor: Colors.amberAccent,
-
-            )
-
-          ],
-
+            label: "Students",
+            tooltip: "Students Details",
+            backgroundColor: Colors.purpleAccent,
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.add_photo_alternate,
+              size: 35,
+            ),
+            label: "Flowers",
+            tooltip: "Flower Images",
+            backgroundColor: Colors.orangeAccent,
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.notifications_active,
+              size: 35,
+            ),
+            label: "Notification",
+            tooltip: "Notification",
+            backgroundColor: Colors.amberAccent,
+          )
+        ],
       ),
-
-
-          );
+    );
   }
 }
